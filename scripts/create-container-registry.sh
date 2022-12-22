b37736a0916f3b24edd4839bc1a4bbcb4f4a7f30
Linus Weigand <linusweigand@protonmail.com>
2022-12-22 16:41:57 +0100

    add create-container-registry script

---
+++ scripts/create-container-registry.sh
@@ -0,0 +1,9 @@
+RESOURCE_GROUP_NAME = "storage-resource-group"
+CONTAINER_REGISTRY_NAME = "containerregistrylinus"
+AKS_CLUSTER_NAME = "akslinus"
+
+# create resource group
+az group create -n $AKS_RESOURCE_GROUP -l $AKS_LOCATION
+
+# create container registry
+az aks create -n $AKS_CLUSTER_NAME -g $AKS_RESOURCE_GROUP --generate-ssh-keys --attach-acr $ACR_NAME
