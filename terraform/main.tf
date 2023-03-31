c6999f4e920687d8611574323882b36afde7eb9e
Linus Weigand <linusweigand@protonmail.com>
2023-03-31 17:57:46 +0200

    format

--- terraform/main.tf
+++ terraform/main.tf
@@ -80,50 +80,50 @@ module "vnet" {
 # }
 
 # Kubernetes Cluster
-#module "aks" {
-#  source                     = "./aks"
-#  name                       = local.cluster_name
-#  location                   = var.location
-#  kubernetes_version         = "1.24.6"
-#  agent_count                = 1
-#  vm_size                    = "Standard_B2s"
-#  ssh_public_key             = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDeKC5qE4+lWfW+QNh2ZuUKKoWh6jXiv2Rm0HyjdPuHiEb67TovTwYL/pfYomU0XDy9zmMsbLVRRm370ITNT0wOby2oZVN/ezaSBf9X2Mlrs4iykuf8715lfJi9xfy+aJFgNsdN28f+YvGPGU2O8KavSKtARoaXgE4wx1TWUHJIjxUw0XDdDdyJXvzQxAXqmOesPjaqq6vJymmM01NgvPBheIg0m93T849CQ8wS4JouhAdznJ+AR+YX/RgK54F9OvgI6HFQg1e4puh2P+tcmTsYiqLGu+Fj8pvK7kjQoLBe4PJFNzf2zh5qrCTkrfu2pg9KoGY2uykDmae2iDgkaAGJYbE2UVk6vcfGXiy9W4UMZXAyQa8vhmInUHAJBnSgcILmUClArgx73e+fJ5dINrW1KaKX+uMpNa751N804ksF9rvK/qZinFOCIW06+zrA0NNrl7Ws5TyZs0mvQYb22mSw0tOOye7uzUBFNwJD79oo2ft9QgbfkgXObW1J6sp+Edk= linus@LAPTOP-FU4LQFR3"
-#  aks_admins_group_object_id = "d0819c2d-cf12-40b7-b2cf-169cff1e2927"
-#  resource_group_name        = var.resource_group_name
-#  akssubnet_id               = module.vnet.akssubnet_id
-#  dns_zone_id                = var.dns_zone_id
-#  node_resource_group        = local.node_resource_group
-#}
+# module "aks" {
+#   source                     = "./aks"
+#   name                       = local.cluster_name
+#   location                   = var.location
+#   kubernetes_version         = "1.24.6"
+#   agent_count                = 1
+#   vm_size                    = "Standard_B2s"
+#   ssh_public_key             = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDeKC5qE4+lWfW+QNh2ZuUKKoWh6jXiv2Rm0HyjdPuHiEb67TovTwYL/pfYomU0XDy9zmMsbLVRRm370ITNT0wOby2oZVN/ezaSBf9X2Mlrs4iykuf8715lfJi9xfy+aJFgNsdN28f+YvGPGU2O8KavSKtARoaXgE4wx1TWUHJIjxUw0XDdDdyJXvzQxAXqmOesPjaqq6vJymmM01NgvPBheIg0m93T849CQ8wS4JouhAdznJ+AR+YX/RgK54F9OvgI6HFQg1e4puh2P+tcmTsYiqLGu+Fj8pvK7kjQoLBe4PJFNzf2zh5qrCTkrfu2pg9KoGY2uykDmae2iDgkaAGJYbE2UVk6vcfGXiy9W4UMZXAyQa8vhmInUHAJBnSgcILmUClArgx73e+fJ5dINrW1KaKX+uMpNa751N804ksF9rvK/qZinFOCIW06+zrA0NNrl7Ws5TyZs0mvQYb22mSw0tOOye7uzUBFNwJD79oo2ft9QgbfkgXObW1J6sp+Edk= linus@LAPTOP-FU4LQFR3"
+#   aks_admins_group_object_id = "d0819c2d-cf12-40b7-b2cf-169cff1e2927"
+#   resource_group_name        = var.resource_group_name
+#   akssubnet_id               = module.vnet.akssubnet_id
+#   dns_zone_id                = var.dns_zone_id
+#   node_resource_group        = local.node_resource_group
+# }
 #
-#module "cert-manager" {
-#  source               = "./cm"
-#  resource_group_name  = var.resource_group_name
-#  cert_manager_version = "v1.11.0"
-#  domain               = var.domain
-#  cluster_name         = local.cluster_name
-#  location             = var.location
+# module "cert-manager" {
+#   source               = "./cm"
+#   resource_group_name  = var.resource_group_name
+#   cert_manager_version = "v1.11.0"
+#   domain               = var.domain
+#   cluster_name         = local.cluster_name
+#   location             = var.location
 #
-#  depends_on = [
-#    module.aks,
-#  ]
-#}
+#   depends_on = [
+#     module.aks,
+#   ]
+# }
 #
-#module "cert-manager-deployments" {
-#  source              = "./cm_deployments"
-#  resource_group_name = var.resource_group_name
-#  domain              = var.domain
-#  email               = var.email
-#  subscription_id     = var.SUBSCRIPTION_ID
-#  location            = var.location
-#  cluster_name        = local.cluster_name
-#  node_resource_group = local.node_resource_group
-#  name                = var.name
-#  namespace           = "ingress-basic"
+# module "cert-manager-deployments" {
+#   source              = "./cm_deployments"
+#   resource_group_name = var.resource_group_name
+#   domain              = var.domain
+#   email               = var.email
+#   subscription_id     = var.SUBSCRIPTION_ID
+#   location            = var.location
+#   cluster_name        = local.cluster_name
+#   node_resource_group = local.node_resource_group
+#   name                = var.name
+#   namespace           = "ingress-basic"
 #
-#  depends_on = [
-#    module.cert-manager
-#  ]
-#}
+#   depends_on = [
+#     module.cert-manager
+#   ]
+# }
 
 # module "ingress-controller" {
 #   source              = "./ing_controller"
