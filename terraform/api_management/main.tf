2a9a4f58d219b2132c6f3f481fe2af135c2f1dba
Linus Weigand <linusweigand@protonmail.com>
2023-04-26 14:02:45 +0200

    add authorization server

--- terraform/api_management/main.tf
+++ terraform/api_management/main.tf
@@ -49,4 +49,25 @@ resource "azurerm_api_management_backend_pool" "backend_pool" {
 }
 
 
+resource "azurerm_api_management_authorization_server" "keycloak_authorization_server" {
+  name                         = "keycloak-authorization-server"
+  resource_group_name          = var.resource_group_name
+  api_management_name          = azurerm_api_management.apim.name
+  display_name                 = "Keycloak Authorization Server"
+  authorization_methods        = ["GET", "POST"]
+  client_authentication_method = ["Basic"]
+  grant_types = [
+    "authorization_code",
+    "implicit",
+    "client_credentials",
+    "password",
+    "refresh_token"
+  ]
+  token_lifetime_in_seconds    = 3600 # 1 hour
+  client_registration_endpoint = "https://keycloak.example.com/auth/realms/my-realm/clients"
+  authorization_endpoint       = "https://keycloak.example.com/auth/realms/my-realm/protocol/openid-connect/auth"  # Get authorization code and id token
+  token_endpoint               = "https://keycloak.example.com/auth/realms/my-realm/protocol/openid-connect/token" # Get access, refresh token
+  end_session_endpoint         = "https://keycloak.example.com/auth/realms/my-realm/protocol/openid-connect/logout"
+  bearer_token_sending_methods = ["authorizationHeader"]
+  default_scope                = "openid"
 }
